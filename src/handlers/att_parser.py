7f68f02621102546509aa7bce5b2e56eb0d41db3
igornet0 <892130836@mail.ru>
2025-11-05 13:45:33 +0300

    Fix bug

--- src/handlers/att_parser.py
+++ src/handlers/att_parser.py
@@ -11,7 +11,7 @@ import os
 from src.parser_driver import (ParserApi, KuCoinAPI, ParserNewsApi, 
                            ParserKucoin, TelegramParser)
 from src.core.models import Dataset, DatasetTimeseries
-from src.core.database.orm import (NewsData, orm_get_coins, orm_get_timeseries_by_coin, 
+from src.core.database.orm import (NewsData, PriceData, orm_get_coins, orm_get_timeseries_by_coin, 
                                     orm_add_timeseries, orm_add_data_timeseries, orm_update_coin_price, 
                                     orm_get_telegram_channels, orm_add_news)
 from src.core.utils import AutoDecorator
